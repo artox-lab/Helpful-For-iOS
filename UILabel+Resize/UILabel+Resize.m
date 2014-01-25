e92807848cf24687bdba8ba3dac1266f85837d42
Ivanenko Dmitry <id.noxt@gmail.com>
2014-01-25 12:11:37 +0300

    Fix bug

--- UILabel+Resize/UILabel+Resize.m
+++ UILabel+Resize/UILabel+Resize.m
@@ -11,23 +11,23 @@
 
 - (void)resizeVertical
 {
-    CGFloat width = self.width;
+    CGFloat width = self.frame.size.width;
     
     [self sizeToFit];
     
     CGSize expectedSize = [self textRectForBounds:self.frame limitedToNumberOfLines:self.numberOfLines].size;
-    self.size = CGSizeMake(width, expectedSize.height);
+    self.frame = CGRectMake(self.frame.origin.x, self.frame.origin.y, width, expectedSize.height);
 }
 
 
 - (void)resizeHorizontal
 {
-    CGFloat height = self.height;
+    CGFloat height = self.frame.size.height;
     
     [self sizeToFit];
     
     CGSize expectedSize = [self textRectForBounds:self.frame limitedToNumberOfLines:self.numberOfLines].size;
-    self.size = CGSizeMake(expectedSize.width, height);
+    self.frame = CGRectMake(self.frame.origin.x, self.frame.origin.y, expectedSize.width, height);
 }
 
 @end
